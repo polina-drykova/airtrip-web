5a55ca6b841c48bbbd80e49044f2c4931a30a5e9
Polina Drykova <polina.drykova1988@gmail.com>
2019-12-12 19:16:38 +0700

    created controller and set root route

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def service
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+end
