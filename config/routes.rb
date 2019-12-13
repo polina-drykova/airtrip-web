0ecc6c9aba55e345c561228e218eb942e0ff1376
Polina Drykova <polina.drykova1988@gmail.com>
2019-12-13 08:35:49 +0700

    created views and set the route for about, contact, service, terms and privacy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'privacy', to: 'pages#privacy', as: :privacy
+  get 'service', to: 'pages#service', as: :service
+  get 'terms', to: 'pages#terms', as: :terms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
